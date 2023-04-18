f30568c65b670be6b9dc3e759333f9190ff5a957
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2023-04-18 21:01:08 +0430

    Finish the bacck end part

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  get 'root/greeting'
-  get 'root/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get '/api/messages', to: 'root#greeting'
 end
