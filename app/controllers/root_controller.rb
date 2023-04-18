261a92fb407d6bf0c2f448690a8d89219f9587d4
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2023-04-18 22:28:10 +0430

    Add condation if greeting is nil

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -1,5 +1,13 @@
 class RootController < ApplicationController
   def greeting
-    render json: { greeting: Greeting.all.sample.text }
+    greeting = Greeting.all.sample
+    if greeting.nil?
+      render json: { error: 'No greeting found'},
+      status: :not_found
+      
+    else
+      render json: { greeting: greeting.text }
+
+    end
   end
 end
