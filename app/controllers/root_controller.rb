9ca05330b35c724edc106da6011e142e722fdab3
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2023-04-19 10:26:02 +0430

    Solve the linter errrors

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -2,9 +2,9 @@ class RootController < ApplicationController
   def greeting
     greeting = Greeting.all.sample
     if greeting.nil?
-      render json: { error: 'No greeting found'},
-      status: :not_found
-      
+      render json: { error: 'No greeting found' },
+             status: :not_found
+
     else
       render json: { greeting: greeting.text }
 
--- test/controllers/root_controller_test.rb
+++ test/controllers/root_controller_test.rb
@@ -1,7 +1,7 @@
-require "test_helper"
+require 'test_helper'
 
 class RootControllerTest < ActionDispatch::IntegrationTest
-  test "should get greeting" do
+  test 'should get greeting' do
     get root_greeting_url
     assert_response :success
   end
